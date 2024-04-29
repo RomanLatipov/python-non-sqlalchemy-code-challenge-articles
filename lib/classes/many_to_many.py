d1ea45dc12828d32db4687aefb14515585f9cb99
romanlatipov <romanlatipov@gmail.com>
2024-04-29 13:36:48 -0400

    complete code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,12 @@
 class Article:
+
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
     
     @property
     def title(self):
@@ -11,29 +15,48 @@ class Article:
     @title.setter
     def title(self, value):
         if hasattr(self, "title"):
-            print("Title cannot be chnaged!")
+            print("Title cannot be changed!")
         elif (len(value) <= 5):
             print("Title is too short")
         elif (len(value) >= 50):
             print("Title is too long")
         else:
             self._title = value
-    
+
 class Author:
-    def __init__(self, name):
+    def __init__(self, name:str):
         self.name = name
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in Article.all if article.author == self]))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        temp = list(set([article.magazine.category for article in Article.all if article.author == self]))
+        if temp == []:
+            return None
+        else:
+            return temp
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if hasattr(self, "name"):
+            print("Name can not be changed")
+        elif type(value) != str:
+            print("Name must be of type string")
+        elif len(value) <= 0:
+            print("Name must be longer than 0 characters")
+        else:
+            self._name = value
 
 class Magazine:
     def __init__(self, name, category):
@@ -41,18 +64,42 @@ class Magazine:
         self.category = category
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in Article.all if article.magazine == self]))
 
     def article_titles(self):
-        pass
+        list_of_titles = [article.title for article in Article.all if article.magazine == self]
+        return None if list_of_titles == [] else list_of_titles
 
     def contributing_authors(self):
-        pass
+        list_of_authors = [article.author for article in Article.all if article.magazine == self]
+        new_list = list(set([author for author in list_of_authors if list_of_authors.count(author) >= 2]))
+        return None if new_list == [] else new_list
 
-author = Author("Carry Bradshaw")
-magazine = Magazine("Vogue", "Fashion")
-article_1 = Article(author, magazine, "How")
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if type(value) != str:
+            print("Name must be of type string")
+        elif len(value) < 2 or len(value) > 16:
+            print("Category must be between 2 and 16 characters long")
+        else:
+            self._name = value
 
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if type(value) != str:
+            print("Category must be of type string")
+        elif len(value) <= 0:
+            print("Category must be greater than 0 characters")
+        else:
+            self._category = value            
